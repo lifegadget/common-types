ba0623d427783705ba26e63c831563d61d2cc2c4
Ken Snyder <ken@ken.net>
2017-08-24 19:23:11 -0700

    transpiled

--- lib/common-types.d.ts
+++ lib/common-types.d.ts
@@ -1,14 +1,26 @@
+/**
+ * A Javascript hash which allows for any set of keys
+ */
 export interface IDictionary<T = any> {
     [key: string]: T;
 }
+/**
+ * A numeric Javascript array
+ */
 export interface INumericArray<T = any> {
     [key: number]: T;
 }
+/** A function for sorting JS arrays */
 export declare type SortingFunction = (a: any, b: any) => number;
+/** a string of the format: "YYYY-MM-DD" */
 export declare type datestring = string;
+/** a string of the format: "HH:mm:ss" */
 export declare type timestring = string;
+/** a string of the format: "UTC" */
 export declare type timezone = string;
+/** string representation of datetime in format of "2016-07-17T13:29:11.652Z" */
 export declare type datetime = string;
+/** unix epoch datetime format (aka, seconds since 1970) */
 export declare type epoch = number;
 export declare enum STAGE {
     prod = "prod",
