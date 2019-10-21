5d66239e8d079f76a598918bb60743070873b9b0
Ken Snyder <ken@ken.net>
2019-10-21 01:28:34 -0500

    S3 typings

--- src/async.ts
+++ src/async.ts
@@ -1,4 +1,4 @@
 /** provides a friendly way to pause execution when using async/await symantics */
-export async function wait(ms: number) {
+export async function wait(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
