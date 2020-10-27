4ea386f80feb78f20c7474cf2e7bb3b51d414c6b
Ken Snyder <ken@ken.net>
2020-10-27 16:18:30 -0700

    added ISO codes for country and principle subdivisions

--- dist/es/basics.d.ts
+++ dist/es/basics.d.ts
@@ -67,6 +67,24 @@ export declare type percentage = number;
 export declare type json = string;
 /** a string blob that represents CSV structured data */
 export declare type csv = string;
+/**
+ * The ISO 2 letter standard for [country codes](https://www.iso.org/iso-3166-country-codes.html).
+ *
+ * You can find a full listing here: https://www.iso.org/obp/ui/#search
+ */
+export declare type Iso3166 = "US" | "CA" | "MX" | "GB" | "DE" | "FR" | string;
+/**
+ * The ISO standard for country principle subdivisions; which
+ * in the US is a State but could be a _provence_, _parish_, etc.
+ *
+ * > **Note:** we use _snake\_case_ instead of the standard ISO convension
+ * of using _dasherization_.
+ */
+export declare type Iso3166_2 = "US_CA" | "US_MI" | "US_AZ" | "US_NV" | string;
+/**
+ * The ISO 3 letter standard for country codes
+ */
+export declare type Iso3166_Alpha3 = "USA" | string;
 /** a _string_ variable which contains **Markdown** content */
 export declare type markdown = string;
 /** a _string_ variable which contains **HTML** content */
