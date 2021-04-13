90418f7fc0ee25e4f62802c08b33ff15f1aaa77e
Ken Snyder <ken@ken.net>
2021-04-12 17:30:53 -0700

    added TypeSubtype type

--- src/aliases/stringAliases.ts
+++ src/aliases/stringAliases.ts
@@ -1,3 +1,8 @@
 export type sql = string;
 
 export type NumericCharacter = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+
+/**
+ * A string representation of the common Type/SubType classification.
+ */
+export type TypeSubtype = `${string}/${string}`;
