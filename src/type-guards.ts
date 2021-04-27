e509f7f65e32ce4972186914284cfa260e3ea465
Ken Snyder <ken@ken.net>
2021-04-27 09:19:15 -0700

    added isNonNullObject type guard

--- src/type-guards.ts
+++ src/type-guards.ts
@@ -6,3 +6,14 @@ import { TypeSubtype } from "./aliases";
 export function isTypeSubtype(input: unknown): input is TypeSubtype {
   return typeof input === "string" && input.split("/").length === 2;
 }
+
+/**
+ * A type guard that checks whether an input is an "object" but also
+ * _not_ `null`.
+ *
+ * This is useful test in its own right but can also be an important
+ * building block for other type guards.
+ */
+export function isNonNullObject<T extends object>(input: unknown): input is T {
+  return typeof input === "object" && input !== null;
+}
